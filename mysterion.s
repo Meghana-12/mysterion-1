bafd016abb030a36dc6c7486080866b5ee5a67fd
Daan Sprenkels <hello@dsprenkels.com>
2017-06-07 17:33:22 +0200

    Implement unbyteslice, key ops, sbox

--- mysterion.s
+++ mysterion.s
@@ -13,51 +13,16 @@
 .syntax unified
 .cpu cortex-m4
 
-
-
-/*
- * arguments:
- * - r0: pointer to input buffer of 16 octets
- * returns:
- * - {rout0 - 3} bitsliced format of rin data
- */
-.macro byteslice rin:req, rout0:req, rout1:req, rout2:req, rout3:req, rtmp:req
-    mov \rout0, #0
-    mov \rout1, #0
-    mov \rout2, #0
-    mov \rout3, #0
-
-
-
-.endm
-
-.macro for code:req
-    \code
-.endm
-
-.macro incl code:req
-    \code
-.endm
-
-
-
-
 .global mysterion
 .type mysterion, %function
-/*
- * arguments:
- * - r0: pointer to the message buffer, the output will be written here
- * - r1: pointer to the key buffer
- * returns: void
- */
-.macro madd i
-  .long \i
-  mov r0, #(((42)+\i)/7)
-.endm
 
 
-byteslice_msg:
-    // Byteslice from msg buffer in 1 chunks of 4, and 2 chunks of 6
+.macro byteslice_msg
+    /*
+     * Byteslice from msg buffer in 1 chunk of 4, and 2 chunks of 6
+     * r0: pointer to msg/state
+     * r2..r5: bytesliced (output) state
+     */
     ldrb r5, [r0, #15]
     ldrb r4, [r0, #14]
     ldrb r3, [r0, #13]
@@ -88,21 +53,131 @@ byteslice_msg:
     orr r4, r4, r8, lsl #24
     orr r3, r3, r7, lsl #24
     orr r2, r2, r6, lsl #24
+.endm
 
-    b mysterion_ret1
+
+.macro byteslice_key
+    /*
+     * Byteslice from key buffer in 1 chunk of 4, and 4 chunks of 3
+     * r1: pointer to key
+     * r6..r9: bytesliced (output) key
+     */
+    ldrb r9, [r1, #15]
+    ldrb r8, [r1, #14]
+    ldrb r7, [r1, #13]
+    ldrb r6, [r1, #12]
+
+    ldrb r12, [r1, #11]
+    ldrb r11, [r1, #10]
+    ldrb r10, [r1, #9]
+    orr r9, r9, r12, lsl #8
+    orr r8, r8, r11, lsl #8
+    orr r7, r7, r10, lsl #8
+
+    ldrb r12, [r1, #8]
+    ldrb r11, [r1, #7]
+    ldrb r10, [r1, #6]
+    orr r6, r6, r12, lsl #8
+    orr r9, r9, r11, lsl #16
+    orr r8, r8, r10, lsl #16
+
+    ldrb r12, [r1, #5]
+    ldrb r11, [r1, #4]
+    ldrb r10, [r1, #3]
+    orr r7, r7, r12, lsl #16
+    orr r6, r6, r11, lsl #16
+    orr r9, r9, r10, lsl #24
+
+    ldrb r12, [r1, #2]
+    ldrb r11, [r1, #1]
+    ldrb r10, [r1, #0]
+    orr r8, r8, r12, lsl #24
+    orr r7, r7, r11, lsl #24
+    orr r6, r6, r10, lsl #24
+.endm
+
 
 .macro unbyteslice_state
-    // r0 is destination buffer
-    // r2-r5 bytesliced input state
-    @mov r11, r5, lsr #24
-    @strb [r0, #15], r5, lsr #24
+    /*
+     * r0 is destination buffer
+     * r2-r5 bytesliced input state (*this input is consumed during execution*)
+     */
+    strb r2, [r0, #12]
+    strb r3, [r0, #13]
+    strb r4, [r0, #14]
+    strb r5, [r0, #15]
 
+    lsr r2, r2, #8
+    lsr r3, r3, #8
+    lsr r4, r4, #8
+    lsr r5, r5, #8
+
+    strb r2, [r0, #8]
+    strb r3, [r0, #9]
+    strb r4, [r0, #10]
+    strb r5, [r0, #11]
+
+    lsr r2, r2, #8
+    lsr r3, r3, #8
+    lsr r4, r4, #8
+    lsr r5, r5, #8
+
+    strb r2, [r0, #4]
+    strb r3, [r0, #5]
+    strb r4, [r0, #6]
+    strb r5, [r0, #7]
+
+    lsr r2, r2, #8
+    lsr r3, r3, #8
+    lsr r4, r4, #8
+    lsr r5, r5, #8
+
+    strb r2, [r0]
+    strb r3, [r0, #1]
+    strb r4, [r0, #2]
+    strb r5, [r0, #3]
+.endm
+
+
+.macro add_key
+    eor r2, r2, r6
+    eor r3, r3, r7
+    eor r4, r4, r8
+    eor r5, r5, r9
+.endm
+
+
+.macro sbox
+    /*
+     * Equivalent Python code:
+     *
+     * a = (state[0] & state[1]) ^ state[2] # Use r10
+     * state[2] = (state[1] | state[2]) ^ state[3]
+     * state[3] = (a & state[3]) ^ state[0]
+     * state[1] = (state[2] & state[0]) ^ state[1] # new state[2] value
+     *            ^~~~~~~~~~~~~~ Use r11
+     * state[0] = a
+     */
+    and r10, r2, r3
+    eor r10, r10, r4
+    orr r4, r4, r3
+    eor r4, r4, r5
+    and r5, r5, r10
+    eor r5, r5, r2
+    and r11, r4, r2
+    eor r3, r3, r11
+    mov r2, r10
 .endm
 
 
 mysterion:
     push {r4-r12}
-    b byteslice_msg
-mysterion_ret1:
+    byteslice_msg
+    byteslice_key
+    /* From this point [r0, r2..r9] are in use */
+    add_key
+    sbox
+    mov r0, r3
+
     pop {r4-r12}
     bx lr
--- wrapper.c
+++ wrapper.c
@@ -1,19 +1,32 @@
 #include "stm32wrapper.h"
 #include <stdio.h>
+#include <inttypes.h>
 
 
-extern int mysterion();
+extern int mysterion(uint8_t* state, uint8_t* key);
 
 
 int main(void)
 {
+    char output[128];
+    size_t i;
+
     clock_setup();
     gpio_setup();
     usart_setup(115200);
 
-    char buf[128];
-    snprintf(buf, sizeof buf, "Hello %i\n", mysterion());
-    send_USART_str(buf);
+    uint8_t state[16] = {0};
+    for (i = 0; i < 16; ++i) {
+        state[i] = i;
+    }
+    uint8_t key[16] = {0};
+    for (i = 0; i < 16; ++i) {
+        key[i] = 42;
+    }
+
+    uint32_t ret = mysterion(state, key);
+    snprintf(output, sizeof output, "'%i'\n", (int) ret);
+    send_USART_str(output);
 
     while(1);
     return 0;
